53b288fad527e702ffa1a2c4bf5ffeef91dd5ee6
jacekpiotrowski89 <143739653+jacekpiotrowski89@users.noreply.github.com>
2024-02-14 00:33:02 +0100

    Update dice_simulator_2024.py
    Adding new option:
    You can add new die and quantity to rolling pull.

--- dice_simulator_2024.py
+++ dice_simulator_2024.py
@@ -4,65 +4,86 @@ import random
 def dice_sides():
     try:
         while True:
-            dice_choice = input('A dice choice(sides): ')
+            dice_choice = input('A die choice(sides): ')
             dice_type = ['4','6','8','10','12','20']
             if dice_choice not in dice_type:
                 print('Only: 4, 6 , 8, 10, 12, 20')
             else:
-                return dice_choice            
-    except:  
-         dice_sides()
-
-def dice_number(dice_choice):
+                global dice_choice_int
+                dice_choice_int = int(dice_choice)
+                return dice_choice_int    
+    except ValueError:
+        print('Value Error')  
+         
+def dice_number():
     while True:
         try:
-            dice_number_choice = input(f'For {dice_choice} dice Enter a quantity(1-100): ')
+            dice_number_choice = input(f'For {dice_choice_int} die Enter a quantity(1-100): ')
+            global dice_number_int
             dice_number_int = int(dice_number_choice)
             if dice_number_int not in range(1,101):
                 print('Only number: min:1 max:100.')
             else:
-                return dice_number_choice              
+                return dice_number_int           
         except ValueError:
             print('Only number: min:1 max:100.')
-              
-def dice_rolling():
-    try:
-        while True:
-            total = 0
-            dice_choice = dice_sides() 
-            dice_choice_int = int(dice_choice)
-            dice_number_choice = dice_number(dice_choice)
-            number_int = int(dice_number_choice)
-            if number_int > 1:
-                print('Rolls: ', end='')
-                for i in range(1, number_int):
-                    roll = random.randint(1, dice_choice_int)
-                    print(f'{roll}', end=', ')
-                    total += roll
-                last_roll = random.randint(1, dice_choice_int)
-                total += last_roll
-                print(f'{last_roll} Total: {total}')     
+
+def add_dice():
+    while True:
+        try:
+            adding_inp = input('Do You want add new die to rolling pool(y/n): ')
+            if adding_inp == 'y':
+                dice_list.append(dice_choice_int)
+                dice_number_list.append(dice_number_int)
+                dice_sides()
+                dice_number()
+            elif adding_inp == 'n':
+                dice_list.append(dice_choice_int)
+                dice_number_list.append(dice_number_int)
+                break
             else:
-                roll =  random.randint(1, dice_choice_int)
-                print(f"Roll: {roll}")
-            quit1()
-    except ValueError:
-        print('Dice rolling Value Error')
-        dice_number(dice_choice)
-   
+                print("Only 'y' or 'n'.")
+        except ValueError:
+            print('Adding Value Error')
+
+def rolling():
+    try:
+        result = []
+        result_end = []
+        for y, z in zip(dice_list, dice_number_list):
+            result = ([y]*z)
+            for x in result:
+                dice = random.randint(1,y)
+                result_end.append(dice)
+        suma = sum(result_end)
+        result_end_str = ", ".join(str(i) for i in result_end)
+        print(f'Rolls: {result_end_str} Total: {suma}')   
+    except:
+        print('Error in calculations.')    
+
 def quit1():    
     try:
-        while True:
-            quit1 = input('\nContinue?(y/n): ' )
-            if quit1 == 'n':
-                print('GOODBYE from Dice Simulator 2024.\n')
-                quit()
-            elif quit1 == 'y':
-                dice_rolling()
-            else:
-                print("Only: 'y' or 'n'") 
+        quit1 = input('\nContinue?(y/n): ' )
+        if quit1 == 'n':
+            print('GOODBYE from Dice Simulator 2024.\n')
+            quit()
+        elif quit1 == 'y':
+            dice_rolling()
+        else:
+            print('Continue?(y/n): ') 
     except ValueError:
         print('Quit Value Error')
+
+def dice_rolling():
+    global dice_list
+    dice_list = []
+    global dice_number_list
+    dice_number_list = []
+    dice_sides()
+    dice_number()
+    add_dice()
+    rolling()
+    quit1()
 
 if __name__ == '__main__':
     os.system('cls')
